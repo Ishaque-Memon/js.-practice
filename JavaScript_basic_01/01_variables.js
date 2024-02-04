d5086cf1a4b0bd419acbf3650000f82b85f3aca6
Muhammad Ishaque <155660769+Ishaque-Memon@users.noreply.github.com>
2024-02-04 15:53:31 +0000

    Variables in js

---
+++ JavaScript_basic_01/01_variables.js
@@ -0,0 +1,16 @@
+let accountEmail = "haseebmeymon@gmail.com"
+const accountId = 144528
+var accountPassword = "pass1234"
+accountCity = "Hyderabad, Sindh"
+
+
+// accountId = 2 not allowed!
+// accountPassword ="1234pass"
+// accountEmail = "hello@gmail.com"
+// accountCity = "Karachi, Sindh"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
