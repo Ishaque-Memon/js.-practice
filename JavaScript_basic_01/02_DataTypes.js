de8c19509b517b8da2414d2f1ef317397e8eb607
Muhammad Ishaque <155660769+Ishaque-Memon@users.noreply.github.com>
2024-02-05 10:02:38 +0000

    Datatypes in js

---
+++ JavaScript_basic_01/02_DataTypes.js
@@ -0,0 +1,9 @@
+"use strict";  //Treat all Js code as newer version. 
+//Note: With strict mode, you can not, for example, use undeclared variables.
+
+//accountId = 2 just like this is undeclared variable
+
+//var accountId = 2 This is correct way to declare an variable
+//console.log(accountId);
+
+// alert(3+3) Note: we are using nodejs, not browser
